0b62d6410550c766ab15d13f2c8730d318c8b87a
zenpoet <anh@anh-thunderbolt.cs.Virginia.EDU>
2016-04-11 13:47:53 -0400

    add home page w/ author credits

---
+++ www/js/foobar.js
@@ -0,0 +1,8 @@
+
+var trees = new Array("redwood", "bay", "cedar", "oak", "maple");
+if ("redwood" in trees)
+	console.log("Yes");
+
+var vowel = ["a", "e", "i", "o", "u"];
+if ('a' in vowel)
+	console.log("Yes vowel");
